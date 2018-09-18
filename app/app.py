fb1e2c6fa865a04fb0b3d536b727c3e8316df6d1
Jonathan Johnston <jonathan.david.johnston@gmail.com>
2018-09-17 20:37:21 -0400

    refactor(heroku): shuffle flask app & web files to app dir

--- app/app.py
+++ app/app.py
@@ -1,14 +1,13 @@
-from flask import (
-    Flask,
-    render_template,
-)
+from flask import Flask
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 if __name__ == "__main__":
     app.run()
-
